73842eb0875333b98bfc15e62b7f1ff21e8d6b5d
Brandon Tu <brandontu@stemworldeducationalservices.com>
2021-07-19 22:08:49 -0400

    added the card views for the side project, more must be done for the links and stuff

---
+++ js/index.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function() {
+    console.log("under construction")
+    document.getElementById("link1").onclick = function(e) {
+        console.log("button clicked");
+    }
+
+});
