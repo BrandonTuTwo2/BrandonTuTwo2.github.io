cee1c3e3b64d1d372c3384921295c68a075bd8ca
Brandon Tu <brandontu@stemworldeducationalservices.com>
2021-08-03 18:34:49 -0400

    added the links for the cards and added a pdf viewer for the resume

--- js/index.js
+++ js/index.js
@@ -1,7 +1,4 @@
 jQuery(document).ready(function() {
     console.log("under construction")
-    document.getElementById("link1").onclick = function(e) {
-        console.log("button clicked");
-    }
 
 });
